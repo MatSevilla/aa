4a3703bc7ffbec1abc19aa4b4d7aa72bc574676b
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-24 22:12:28 -0700

    animalsnames

---
+++ animalsnames.py
@@ -0,0 +1,8 @@
+animals = ["cat", "dog", "sheep", "lion", "giraffe", "pig", "duck", "chicken", "rat", "fish",]
+print(animals)
+print("3rd index", animals[2])
+animals [5] = "Elephant"
+print(animals)
+del animals[7]
+print(animals)
+print("last animals index is ",animals[2])
