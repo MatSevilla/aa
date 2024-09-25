431a86abf6b3ac41f23a7fbc4afa7eeadada0c2e
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-24 22:31:52 -0700

    colorsname.py

---
+++ colors.py
@@ -0,0 +1,8 @@
+colors = ["yellow", "red", "pink", "blue", "green", "black", "brown", "gray", "ligh blue", "orange", "white", "indigo", "teal", "yellow green"]
+print(colors)
+print("9th index is ", colors[9])
+colors[3] = "purple"
+print(colors)
+del colors [4]
+print(colors[1:8])
+print("Last index is",colors[-1])
