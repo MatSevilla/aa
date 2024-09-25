4cc4799d47c74b6c8931d1c2210e159d03909655
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-24 22:57:13 -0700

    fruit_list

---
+++ fruit_list.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "Guava", "coconut", "durian", "cherry", "papaya", "orange", "santol", "melon", "watermelon","mango"]
+print(fruits)
+print("9th index is",fruits[8])
+fruits[1] = "mango"
+print(fruits)
+del fruits[9]
+print(fruits)
+print(fruits[3:7])
+print("the last index is",fruits[-1] )
