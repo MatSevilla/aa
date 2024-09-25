4366d09b96449098e4caaf5fbad84e266cf27cd5
mat gabriel sevilla <matsevilla2@gmail.com>
2024-09-24 21:31:03 -0700

    studentname.py

---
+++ studentsname.py
@@ -0,0 +1,12 @@
+names = ["MAT", "GAB", "RAI", "BAI", "RON", "JUN", "SAI", "LEE", "BARRY", "BRUCE", "LUCY", "ACE", "BARO", "WAYNE", "SENJI", "KANE", "DREI", "CHRIS", "NOAH", "DEX"]
+
+print(names)
+print("15 Index is ", names[14])
+
+names[11] = "JUNJUN"
+print(names)
+
+del names[2]
+print(names)
+print(names[1:5])
+print("last index is",names[-1])
